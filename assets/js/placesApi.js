0f712d0ff7fae75aca20f62d1fb96f6c74b97431
Hannah <hannmarrk96@gmail.com>
2018-04-08 00:09:17 -0400

    git commit

--- assets/js/placesApi.js
+++ assets/js/placesApi.js
@@ -1,18 +1,68 @@
-    // latitude,longitude
-        var loc = "40.939252,-74.124226";
-        //google api key
-        var apiKey = "AIzaSyC2HtqDacJ12JKefj4ooXduCb9YkmZczHg";
-        //Kevin's cors workaround server
-        var queryURL = "https://mighty-river-19291.herokuapp.com/cors"
-        $.ajax({
-            //post to kevins server
-            method: "POST",
-            url: queryURL,
-            data: {
-                url:"https://maps.googleapis.com/maps/api/place/search/json?query=liquor_store&location=" + loc + "&radius=100&key=" + apiKey,
-                //kevins key, expires at the end of cohort
-                key:"8b5dcaf7cdfb9c46221d492eec6560c571d6ec218b2485c54075ab7840fa77f9"
+var map;
+var infoWindow;
+
+
+
+function initMap(loc1) {
+    // Create the map.
+    var loc = loc1;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: loc,
+        zoom: 17
+    });
+
+    // Create the places service.
+    var service = new google.maps.places.PlacesService(map);
+    infoWindow = new google.maps.InfoWindow();
+
+
+
+    // Perform a nearby search.
+    service.nearbySearch(
+        { location: loc, radius: 500, type: ['liquor_store'] },
+        function (results, status, pagination) {
+            if (status !== 'OK') return;
+
+            for (var i = 0; i < results.length; i++) {
+
+                request = {
+                    placeId: results[i].place_id
+                }
+
+                service.getDetails(request, placeDetailsCallback);
+
+                function placeDetailsCallback(place, status) {
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        createMarker(place);
+                    }
+                }
             }
-        }).then(function (response) {
-            console.log(response);
+
         });
+}
+
+function createMarker(place) {
+
+    //add markers
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name,
+    });
+
+    //expand marker when clicked
+    google.maps.event.addListener(marker, 'click', function () {
+        var name = "<p><strong> " + place.name + "</strong></br>";
+        var address = "Store Address: " + place.formatted_address + "</p>";
+        infoWindow.setContent(name+address);
+        infoWindow.open(map, this);
+    });
+
+    // $("#test").on("click",function() {
+    //     console.log("click")
+    //     // initMap();
+    //     // loc = $(this).attr({ dataLat, dataLong });
+        
+    //   });
+
+}
