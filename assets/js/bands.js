e5de0efa43797695c6386f2bbb0a02b82d8df435
Hannah <hannmarrk96@gmail.com>
2018-04-08 00:09:17 -0400

    git commit

--- assets/js/bands.js
+++ assets/js/bands.js
@@ -1,18 +1,62 @@
 // testing
-
+$(document).ready(function() {
   function searchBandsInTown(artist) {
-
-    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    var queryURL =
+      "https://rest.bandsintown.com/artists/" +
+      artist +
+      "/events?app_id=06eb39d6496f7c099e8fb74c69111298";
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
-// here is to query events /events 
-
-      // Printing the entire object to console
       console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        // Logging and debugging
+        console.log("this is the artist name: " + response[i].lineup);
+        console.log("this is the venue name: " + response[i].venue.name);
+        console.log(
+          "this is the venue latitude: " + response[i].venue.latitude
+        );
+        console.log(
+          "this is the venue longitude: " + response[i].venue.longitude
+        );
+        console.log("this is the event link: " + response[i].url);
+        // created a bunch of variables to populate the html as well as ready the lat and long for the liqour api
+        // that we end up matching to
+        var artistName = response[i].lineup;
+        var venueName = response[i].venue.name;
+        var venueLat = response[i].venue.latitude;
+        var venueLong = response[i].venue.longitude;
+        var eventURL = response[i].url;
+        var locBand = { lat: venueLat, lng: venueLong };
+        // these are plugging the results to html
+        var p1 = $("<p>").text(venueName);
+        var mapsBut = $(
+          "<button class=mapsbutton>Get Drunk</button>"
+        );
+        mapsBut.attr({ dataLat: venueLat, dataLong: venueLong });
 
+        mapsBut.on("click", function() {
+          console.log($(this).attr(dataLat));
+          // loc = $(this).attr({dataLat, dataLong });
+          // initMap(loc);
+        });
+        var pB = $("<hr>");
+        var p2 = $("<a>")
+          .attr("href", eventURL)
+          .text("Click to buy tickets");
+        $("#featured-div").append(p1, p2, mapsBut, pB);
+      }
     });
-}
+  }
+
+  // This event reads the artist entered to the search form when user hits search
+  // button and calls the main bands events function
+  $("#search-btn").on("click", function(event) {
+    event.preventDefault();
+    var inputBand = $("#searchBand")
+      .val()
+      .trim();
+    searchBandsInTown(inputBand);
+  });
+});
